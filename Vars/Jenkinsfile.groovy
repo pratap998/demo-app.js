5fe571981906e62b007d532fad7cf6ed2dd39a42
root <root@jenkins.us-west4-b.c.dynamic-cove-356406.internal>
2022-09-06 07:35:47 +0000

    jenkins file change

---
+++ Vars/Jenkinsfile.groovy
@@ -0,0 +1,25 @@
+vars
+| --- Jenkinsfile.groovy
+ 
+// Jenkinsfile.groovy
+def call(String repoUrl) {
+
+pipeline {
+    agent any
+    
+    stages {
+        stage('Build image') {
+            steps {
+              sh "docker build -t demo-app.js ."
+            }
+        }
+        stage('image scanning') {
+            steps {
+              sh "trivy image demo-app.js > scanreport.txt"
+            }
+        }
+        
+            
+        }
+}
+    
