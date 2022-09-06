c4e2d6072835674227f21e5baae9dde8b6ee7ae2
pratap998 <103648347+pratap998@users.noreply.github.com>
2022-09-06 17:12:11 +0530

    Update and rename sourceCode.groovy to sourcecheckout.groovy

--- vars/sourcecheckout.groovy
+++ vars/sourcecheckout.groovy
@@ -6,7 +6,7 @@ echo ""
 echo ""
    if(giturl.contains("github.com"))
    {
-   checkout([$class: 'GitSCM', branches: [[name:branch]], extensions: [], userRemoteConfigs: 
+   checkout ([$class: 'GitSCM', branches: [[name:branch]], extensions: [], userRemoteConfigs: 
            [[credentialsId: 'github', url: giturl]]])
    }
 else
